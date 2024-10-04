5f27432a8407d86e155f250a4d8becf037fd368f
Nitinkumar Aswar <nitinkumaraswar94@gmail.com>
2024-10-04 05:38:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi Nitinkumar How are you
         </a>
       </header>
     </div>
